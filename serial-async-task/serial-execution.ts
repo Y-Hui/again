50d8a5fba8af9c569ec605531913d95c0469020c
Y-Hui <y-hui.mail@foxmail.com>
2022-09-14 14:10:14 +0800

    ✨ feat(serial-execution): 新增 onTaskFinish 回调

--- serial-async-task/serial-execution.ts
+++ serial-async-task/serial-execution.ts
@@ -10,6 +10,18 @@ export interface SerialExecutionOptions<T> {
    */
   alwaysResolve?: boolean
   /**
+   * 当一个任务结束后调用，不论成功或失败
+   *
+   * @param isResolve 该任务是否被 resolve
+   * @param index 任务所在任务列表中的索引位置
+   * @param task  对应的任务
+   */
+  onTaskFinish?: (
+    isResolve: boolean,
+    index: number,
+    task: () => Promise<T>,
+  ) => void
+  /**
    * 某个任务被 reject 时调用
    * @param index 任务所在任务列表中的索引位置
    * @param task  对应的任务
@@ -31,7 +43,7 @@ export interface SerialExecutionOptions<T> {
  * const asyncTask3 = () => {} // 异步任务
  *
  * const task = [asyncTask, asyncTask2, asyncTask3]
- * serialAsyncTask(task).then(() => {
+ * serialExecution(task).then(() => {
  *   // 所有异步任务结束后
  * })
  * ```
@@ -40,9 +52,11 @@ async function serialExecution<T>(options: SerialExecutionOptions<T>) {
   const {
     taskList,
     alwaysResolve = false,
+    onTaskFinish,
     onTaskReject,
     onTaskResolve,
   } = options
+
   const result: T[] = []
   await taskList.reduce(async (res, task, index) => {
     await res
@@ -50,9 +64,11 @@ async function serialExecution<T>(options: SerialExecutionOptions<T>) {
       const val = await task()
       result.push(val)
       onTaskResolve && onTaskResolve(val, index)
+      onTaskFinish && onTaskFinish(true, index, task)
       return Promise.resolve()
     } catch (error) {
       onTaskReject && onTaskReject(error, index, task)
+      onTaskFinish && onTaskFinish(false, index, task)
       if (alwaysResolve) {
         return Promise.resolve()
       }
